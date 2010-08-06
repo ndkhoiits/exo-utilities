95f9f1810d251fa74652ef43a2154cf83df1ed5b
vstorm83 <vstorm83@3e5d066b-cb39-62c2-c181-8bc5aa994189>
2010-08-06 07:00:36 +0000

    Modify SettingMarkupHeadElementPortlet.java
    git-svn-id: https://exo-utilities.googlecode.com/svn@20 3e5d066b-cb39-62c2-c181-8bc5aa994189

--- portlets/286-compliant-portlet-samples/src/main/java/t3/samples/portlets/markup/headers/SettingMarkupHeadElementPortlet.java
+++ portlets/286-compliant-portlet-samples/src/main/java/t3/samples/portlets/markup/headers/SettingMarkupHeadElementPortlet.java
@@ -4,6 +4,7 @@ import org.w3c.dom.Element;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 import javax.portlet.GenericPortlet;
 import javax.portlet.MimeResponse;
@@ -20,15 +21,85 @@ public class SettingMarkupHeadElementPortlet extends GenericPortlet {
    {
       response.setContentType("text/html");
       PrintWriter w = response.getWriter();
-      w.write("<div class=\"portlet-section-header\">You should see <i>\"Setting Markup Head Element Portlet in VIEW mode\"</i> text in the page's title</div>");
+      w.write("<div class=\"portlet-section-header\">Add following elements to Html head</div>");                                    
 
+      w.write("<div class=\"portlet-section-header\">" + 
+         encode("<title>Setting Markup Head Element Portlet in VIEW mode</title>") + "</div>");
+      w.write("<br\\>");
+      
+      w.write("<div class=\"portlet-section-header\">" + 
+         encode("<base href=\"base1\" _target=\"test\">") + "</div>");
+      w.write("<br\\>");
+      
+      w.write("<div class=\"portlet-section-header\">" + 
+         encode("<base href=\"base2\" _target=\"test\">")+ "</div>");
+      w.write("<br\\>");
+      
+      w.write("<div class=\"portlet-section-header\">" + 
+         encode("<link type=\"text/css\" rel=\"stylesheet\" href=\"mystyle.css\">") + "</div>");
+      w.write("<br\\>");
+      
+      w.write("<div class=\"portlet-section-header\">" + 
+         encode("<style type=\"text/css\">body {background-color:yellow}</style>") + "</div>");
+      w.write("<br\\>");
+      
+      w.write("<div class=\"portlet-section-header\">" + 
+         encode("<meta name=\"Description\" content=\"Test add html head elements\" />") + "</div>");
+      w.write("<br\\>");
+      
+      w.write("<div class=\"portlet-section-header\">" +  
+         encode("<script type=\"text/javascript\">alert([milliseconds])</script>") + "</div>");
+      w.write("<br\\>");
+      
       PortletURL renderURL = response.createRenderURL();
       renderURL.setPortletMode(PortletMode.EDIT);
       w.write("<p> Click <a href=\"" + renderURL.toString()
          + "\">here</a> to switch to EDIT mode");
+      addExtraHtmlHead(response);
+   }
+   
+   private String encode(String text) 
+   {
+      text = text.replaceAll("<", "&lt;");
+      return text.replaceAll(">", "&gt;");
+   }
+
+   private void addExtraHtmlHead(RenderResponse response)
+   {
       Element element = response.createElement("title");
       element.setTextContent("Setting Markup Head Element Portlet in VIEW mode");
       response.addProperty(MimeResponse.MARKUP_HEAD_ELEMENT, element);
+      
+      Element base1 = response.createElement("base");
+      base1.setAttribute("href", "base1");
+      base1.setAttribute("_target", "test");             
+      response.addProperty(MimeResponse.MARKUP_HEAD_ELEMENT, base1);
+      
+      Element base2 = response.createElement("base");
+      base2.setAttribute("href", "base2");
+      base2.setAttribute("_target", "test");             
+      response.addProperty(MimeResponse.MARKUP_HEAD_ELEMENT, base2);
+      
+      Element link = response.createElement("link");
+      link.setAttribute("rel", "stylesheet");
+      link.setAttribute("type", "text/css");
+      link.setAttribute("href", "mystyle.css");
+      response.addProperty(MimeResponse.MARKUP_HEAD_ELEMENT, link);
+      
+      Element style = response.createElement("style");
+      style.setAttribute("type", "text/css");
+      style.setTextContent("body {background-color:yellow}");
+      response.addProperty(MimeResponse.MARKUP_HEAD_ELEMENT, style);
+      
+      Element meta = response.createElement("meta");
+      meta.setAttribute("name", "description");
+      meta.setAttribute("content", "Test add html head elements");
+      response.addProperty(MimeResponse.MARKUP_HEAD_ELEMENT, meta);              
+      
+      Element script = response.createElement("script");
+      script.setAttribute("type", "text/javascript");
+      script.setTextContent("alert('" + String.valueOf(new Date().getTime()) + "')");
+      response.addProperty(MimeResponse.MARKUP_HEAD_ELEMENT, script);
    }
 
    @Override
@@ -36,14 +107,11 @@ public class SettingMarkupHeadElementPortlet extends GenericPortlet {
    {
       response.setContentType("text/html");
       PrintWriter w = response.getWriter();
-      w.write("<div class=\"portlet-section-header\">You should see <i>\"Setting Markup Head Element Portlet in EDIT mode\"</i> text in the page's title</div>");
+      w.write("<div class=\"portlet-section-header\">All extra headers should be removed now</div>");
 
       PortletURL renderURL = response.createRenderURL();
       renderURL.setPortletMode(PortletMode.VIEW);
       w.write("<p> Click <a href=\"" + renderURL.toString()
          + "\">here</a> to switch to VIEW mode");
-      Element element = response.createElement("title");
-      element.setTextContent("Setting Markup Head Element Portlet in EDIT mode");
-      response.addProperty(MimeResponse.MARKUP_HEAD_ELEMENT, element);
    }
 }
